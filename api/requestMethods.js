0af5fbd89f721b9b45ceefbac1eaa73fca2c0283
Sinan Şık <sinan.sk@outlook.com.tr>
2024-10-02 23:35:07 +0300

    [v2] - api folder console clear

--- api/requestMethods.js
+++ api/requestMethods.js
@@ -1,5 +1,5 @@
 const axios = require("axios");
-const FATSECRET_AUTH_URL = "https://oauth.fatsecret.com/connect/token"
+const FATSECRET_AUTH_URL = "https://oauth.fatsecret.com/connect/token";
 const FATSECRET_BASE_URL = "https://platform.fatsecret.com/rest/server.api";
 const clientID = process.env.FATSECRET_CLIENT_ID;
 const clientSecret = process.env.FATSECRET_CLIENT_SECRET;
@@ -7,63 +7,64 @@ let cachedToken = null;
 let tokenExpirationTime = null;
 //search food//
 const searchFoodRequest = async (query) => {
-    await getToken()
-    console.log("sarchFoodToken", cachedToken)
-    const method = "foods.search";
-    const format = "json";
-    let page_number;
-    let max_results;
+  await getToken();
+  const method = "foods.search";
+  const format = "json";
+  let page_number;
+  let max_results;
 
-    const url = `${FATSECRET_BASE_URL}?method=${method}&search_expression=${encodeURIComponent(query)}&format=${format}`;
-    console.log(url)
-    try {
-        const response = await axios.post(url, {}, {
-            headers: {
-                Authorization: `Bearer ${cachedToken.access_token}`,
-                Host: "platform.fatsecret.com"
-            }
-        });
-        console.log(response, "search")
-        return response.data;
-    } catch (error) {
-        throw new Error(error);
-    }
+  const url = `${FATSECRET_BASE_URL}?method=${method}&search_expression=${encodeURIComponent(query)}&format=${format}`;
+  try {
+    const response = await axios.post(
+      url,
+      {},
+      {
+        headers: {
+          Authorization: `Bearer ${cachedToken.access_token}`,
+          Host: "platform.fatsecret.com",
+        },
+      }
+    );
 
-}
+    return response.data;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
 
 //GET FATSECRET TOKEN WITH MY DEVELOPER ACCOUNT//
 const getToken = async () => {
-    //check if token expired//
-    console.log("cachedToken", cachedToken)
-    if (cachedToken && tokenExpirationTime > Date.now()) {
-        return cachedToken
-    }
-    const options = {
-        method: 'post',
-        url: 'https://oauth.fatsecret.com/connect/token',
-        auth: {
-            username: clientID,
-            password: clientSecret,
-        },
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        data: 'grant_type=client_credentials&scope=basic',
-    };
+  //check if token expired//
 
-    try {
-        const response = await axios(options);
-        const token = response.data
-        cachedToken = token;
-        tokenExpirationTime = Date.now() + (token.expires_in * 1000) - 60000;
-        return token; // Token'i döndürüyoruz.
-    } catch (error) {
-        throw new Error(error);
-        // res.status(500).json({ error: 'An error occurred' });
-    }
+  if (cachedToken && tokenExpirationTime > Date.now()) {
+    return cachedToken;
+  }
+  const options = {
+    method: "post",
+    url: "https://oauth.fatsecret.com/connect/token",
+    auth: {
+      username: clientID,
+      password: clientSecret,
+    },
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    data: "grant_type=client_credentials&scope=basic",
+  };
+
+  try {
+    const response = await axios(options);
+    const token = response.data;
+    cachedToken = token;
+    tokenExpirationTime = Date.now() + token.expires_in * 1000 - 60000;
+    return token; // Token'i döndürüyoruz.
+  } catch (error) {
+    throw new Error(error);
+    // res.status(500).json({ error: 'An error occurred' });
+  }
 };
 
 module.exports = {
-    searchFoodRequest,
-    getToken,
+  searchFoodRequest,
+  getToken,
 };
